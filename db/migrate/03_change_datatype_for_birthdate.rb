c348f662eac1616357487d8bcba71a6f3e3cac4f
Princeton Rose <decoyzpr@gmail.com>
2019-03-24 21:42:15 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,14 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
 
+  def up
+  end
+
+  def down
+  end
+
+  def change
+    remove_column :students, :birthdate
+    add_column :students, :birthdate, :datetime
+  end
 
 end

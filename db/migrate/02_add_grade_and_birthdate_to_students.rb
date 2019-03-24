4a6aabe8335bc8a1e3953ea5d625dd252dff5777
Princeton Rose <decoyzpr@gmail.com>
2019-03-24 21:18:35 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,11 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
 
+  def up
+  end
+
+  def down
+  end
+
   def change
     add_column :students, :grade, :integer
     add_column :students, :birthdate, :string
